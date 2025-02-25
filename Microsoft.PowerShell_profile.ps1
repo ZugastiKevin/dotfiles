f13420405274083995b9b9556c7167805df9390b
ZugastiKevin <bearded-developer@protonmail.com>
2025-02-25 19:22:07 +0100

    add all dotfiles

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,73 @@
+Invoke-Expression (&starship init powershell)
+
+function eza-l {
+    eza --icons
+}
+
+set-alias l eza-l
+
+function eza-ls {
+    eza --icons
+}
+
+set-alias ls eza-ls
+
+function eza-ll {
+    eza -lg --icons
+}
+
+set-alias ll eza-ll
+
+function eza-la {
+    eza -lag --icons
+}
+
+set-alias la eza-la
+
+function eza-lt {
+    eza -lTg --icons
+}
+
+set-alias lt eza-lt
+
+function eza-lt1 {
+    eza -lTg --level=1 --icons
+}
+
+set-alias lt1 eza-lt1
+
+function eza-lt2 {
+    eza -lTg --level=2 --icons
+}
+
+set-alias lt2 eza-lt2
+
+function eza-lt3 {
+    eza -lTg --leve=3 --icons
+}
+
+set-alias lt3 eza-lt3
+
+function eza-lta {
+    eza -lTag --icons
+}
+
+set-alias lta eza-lta
+
+function eza-lta1 {
+    eza -lTag --level=1 --icons
+}
+
+set-alias lta1 eza-lta1
+
+function eza-lta2 {
+    eza -lTag --level=2 --icons
+}
+
+set-alias lta2 eza-lta2
+
+function eza-lta3 {
+    eza -lTag --level=3 --icons
+}
+
+set-alias lta3 eza-lta3
